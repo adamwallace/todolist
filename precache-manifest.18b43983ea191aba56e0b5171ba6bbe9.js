287b950e37a0d73f17a37b8d2ca8a4119042d00f
Adam Wallace <adam.wallace@calabrio.com>
2019-01-03 16:41:08 -0600

    Updates

---
+++ precache-manifest.18b43983ea191aba56e0b5171ba6bbe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "be66ccb2446cca65f91b",
+    "url": "/todolist/static/js/main.be66ccb2.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/todolist/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "be66ccb2446cca65f91b",
+    "url": "/todolist/static/css/main.71c5abab.chunk.css"
+  },
+  {
+    "revision": "91207dfee4554aa081371da55e9301bd",
+    "url": "/todolist/index.html"
+  }
+];
